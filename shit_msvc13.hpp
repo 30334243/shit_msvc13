c1801aab4ab4313671d2895493df6941674fb7d1
bvs <baboshinvs@spo-department.ru>
2023-04-19 17:39:57 +0300

    переделаны OutOfRange

--- shit_msvc13.hpp
+++ shit_msvc13.hpp
@@ -5,11 +5,26 @@
 #include <cstdint>
 #include <vector>
 #include <fstream>
+#include <memory>
+#include <cmath>
 
+// SHIT
 namespace Shit {
+	uint8_t* gpbeg = nullptr;
+	uint8_t* gpend = nullptr;
 	// CONSTANTS
 	static uint8_t const kSig{2};
 	static uint8_t const kLSig{4};
+	static std::vector<uint8_t> bits{
+		0,
+			0b00000001,
+			0b00000011,
+			0b00000111,
+			0b00001111,
+			0b00011111,
+			0b00111111,
+			0b01111111
+	};
 	// USING
 	using Func = std::function<bool(uint8_t**,uint8_t**)>;
 	// READ
@@ -40,35 +55,74 @@ namespace Shit {
 		}
 	// SHIFT RIGHT
 	static auto Shr(size_t const offset) -> Func {
-			return [offset] (uint8_t** ppbeg, uint8_t** ppend) {
-				(*ppbeg) += offset;
-				return true;
-			};
-		}
+		return [offset] (uint8_t** ppbeg, uint8_t** ppend) {
+			(*ppbeg) += offset;
+			return true;
+		};
+	}
 	// SHIFT LEFT
 	static auto Shl(size_t const offset) -> Func {
-			return [offset] (uint8_t** ppbeg, uint8_t** ppend) {
-				(*ppbeg) -= offset;
-				return true;
-			};
-		}
+		return [offset] (uint8_t** ppbeg, uint8_t** ppend) {
+			(*ppbeg) -= offset;
+			return true;
+		};
+	}
+	// SHIFT LEFT IN BITS
+	static auto ShlInBits(size_t const offset) -> Func {
+		return [offset] (uint8_t** ppbeg, uint8_t** ppend) {
+			uint8_t* pbeg{*ppbeg};
+			size_t const offset_in_bits{offset <= 8 ? 1 : offset / 8};
+			uint8_t const offset_in_bytes{(uint8_t)(offset - offset_in_bits)};
+			while (pbeg < *ppend && (pbeg+1) < *ppend) {
+				*pbeg = ((*pbeg) << offset_in_bits) | ((*(pbeg+1)) & bits[offset_in_bits]);
+				++pbeg;
+			}
+			pbeg -= offset_in_bytes;
+			if (pbeg < *ppend) {
+				*pbeg = (*pbeg) << offset_in_bits;
+				++pbeg;
+			}
+			return true;
+		};
+	}
+	// SHIFT RIGHT IN BITS
+	static auto ShrInBits(size_t const offset) -> Func {
+		return [offset] (uint8_t** ppbeg, uint8_t** ppend) {
+			uint8_t* pbeg{*ppbeg};
+			uint8_t* tmp_pbeg{pbeg};
+			size_t const offset_in_bits{offset <= 8 ? 1 : offset / 8};
+			uint8_t const offset_in_bytes{(uint8_t)(offset - offset_in_bits)};
+			*tmp_pbeg = ((*tmp_pbeg) >> offset_in_bits) | (*(*ppend) & bits[offset_in_bits]);
+			while (pbeg < *ppend && (pbeg+1) < *ppend) {
+				*tmp_pbeg = ((*(tmp_pbeg+1)) >> offset_in_bits) | ((*tmp_pbeg) & bits[offset_in_bits]);
+				++tmp_pbeg;
+			}
+			if (tmp_pbeg < *ppend) {
+				*tmp_pbeg = (*tmp_pbeg) << offset_in_bits;
+			}
+			pbeg += offset_in_bytes;
+			return true;
+		};
+	}
 	// CROP
 	static auto Crop(size_t const skip_beg, size_t const skip_end = 0) -> Func {
-			return [skip_beg, skip_end] (uint8_t** ppbeg, uint8_t** ppend) {
-				uint8_t* pbeg{*ppbeg};
-				pbeg += skip_beg;
-				*ppbeg = pbeg;
-				uint8_t* pend{*ppend};
-				pend -= skip_end;
-				*ppend = pend;
-				return true;
-			};
-		}
+		return [skip_beg, skip_end] (uint8_t** ppbeg, uint8_t** ppend) {
+			uint8_t* pbeg{*ppbeg};
+			pbeg += skip_beg;
+			*ppbeg = pbeg;
+			uint8_t* pend{*ppend};
+			pend -= skip_end;
+			*ppend = pend;
+			return true;
+		};
+	}
+	// SAVE TO LID
 	static void SaveToLid(uint8_t const mask,
 								 uint64_t& lid,
 								 uint8_t const offset) {
 		*(((uint8_t*)&lid)+offset) = mask;
 	}
+	// SAVE TO LID
 	template<class T>
 		static auto SaveToLid(uint64_t& lid, uint8_t& offset) ->
 		std::function<void(T const)> {
@@ -77,6 +131,7 @@ namespace Shit {
 				offset += sizeof(T);
 			};
 		}
+	// FILTER
 	template<class T, class Save>
 		static auto Filter(T const mask, T const val, Save save) -> Func {
 			return [mask, val, save] (uint8_t** ppbeg, uint8_t** ppend) {
@@ -92,8 +147,11 @@ namespace Shit {
 				return ret;
 			};
 		}
+	// FILTER
 	template<class T, class Save>
-		static auto Filter(T const mask, std::vector<uint64_t> const& vec, Save save) -> Func {
+		static auto Filter(T const mask,
+								 std::vector<uint64_t> const& vec,
+								 Save save) -> Func {
 			return [mask, vec, save] (uint8_t** ppbeg, uint8_t** ppend) {
 				bool ret{};
 				for (auto& val : vec) {
@@ -102,12 +160,98 @@ namespace Shit {
 				return ret;
 			};
 		}
-
+	// FILTER NOT
+	template<class T, class Save>
+		static auto FilterNot(T const mask, T const val, Save save) -> Func {
+			return [mask, val, save] (uint8_t** ppbeg, uint8_t** ppend) {
+				bool ret{};
+				uint8_t* pbeg{*ppbeg};
+				T const cur{(T)((*(T*)pbeg) & mask)};
+				if (cur != val) {
+					save(cur);
+					ret = true;
+				} else {
+					ret = false;
+				}
+				return ret;
+			};
+		}
+	// FILTER NOT
+	template<class T, class Save>
+		static auto FilterNot(T const mask,
+									 std::vector<uint64_t> const& vec,
+									 Save save) -> Func {
+			return [mask, vec, save] (uint8_t** ppbeg, uint8_t** ppend) {
+				bool ret{true};
+				for (auto& val : vec) {
+					ret &= FilterNot<T>(mask, (T)val, save)(ppbeg, ppend);
+				}
+				return ret;
+			};
+		}
+	// EQUAL
+	template<class T, class Save>
+		static auto Eq(T const val, Save save) -> Func {
+			return [val, save] (uint8_t** ppbeg, uint8_t** ppend) {
+				bool ret{};
+				uint8_t* pbeg{*ppbeg};
+				T const cur{(T)((*(T*)pbeg))};
+				if (cur == val) {
+					save(cur);
+					ret = true;
+				} else {
+					ret = false;
+				}
+				return ret;
+			};
+		}
+	// EQUAL
+	template<class T, class Save>
+		static auto Eq(std::vector<uint64_t> const& vec,
+							Save save) -> Func {
+			return [vec, save] (uint8_t** ppbeg, uint8_t** ppend) {
+				bool ret{};
+				for (auto& val : vec) {
+					ret |= Eq<T>((T)val, save)(ppbeg, ppend);
+				}
+				return ret;
+			};
+		}
+	// EQUAL NOT
+	template<class T, class Save>
+		static auto EqNot(T const val, Save save) -> Func {
+			return [val, save] (uint8_t** ppbeg, uint8_t** ppend) {
+				bool ret{};
+				uint8_t* pbeg{*ppbeg};
+				T const cur{(T)((*(T*)pbeg))};
+				if (cur != val) {
+					save(cur);
+					ret = true;
+				} else {
+					ret = false;
+				}
+				return ret;
+			};
+		}
+	// EQUAL NOT
+	template<class T, class Save>
+		static auto EqNot(std::vector<uint64_t> const& vec,
+								Save save) -> Func {
+			return [vec, save] (uint8_t** ppbeg, uint8_t** ppend) {
+				bool ret{true};
+				for (auto& val : vec) {
+					ret &= EqNot<T>((T)val, save)(ppbeg, ppend);
+				}
+				return ret;
+			};
+		}
+	// EMPTY
 	static auto Empty() -> std::function<bool(uint8_t**,uint8_t**)> {
 		return [] (uint8_t**,uint8_t**) {
 			return true;
 		};
 	}
+	// MASK
 	template<class T, class Save>
 		static auto Mask(T const mask, Save save) -> Func {
 			return [mask, save] (uint8_t** ppbeg, uint8_t** ppend) {
@@ -124,40 +268,71 @@ namespace Shit {
 				return ret;
 			};
 		}
-	// MASK 3
+	// MASK NOT
 	template<class T, class Save>
-		static auto Mask(Save save) -> std::function<Func(T const)> {
-			return [save] (T const mask) {
-				return [save, mask] (uint8_t** pbeg, uint8_t** pend) {
-					return Mask(mask, save);
-				};
+		static auto MaskNot(T const mask, Save save) -> Func {
+			return [mask, save] (uint8_t** ppbeg, uint8_t** ppend) {
+				bool ret{};
+				uint8_t* pbeg{*ppbeg};
+				T const cur{(T)((*(T*)pbeg) & mask)};
+				if (!cur) {
+					save(0xFF);
+					ret = true;
+				} else {
+					save(0x00);
+					ret = false;
+				}
+				return ret;
 			};
 		}
+	// CLEAN OFFSET
 	static auto CleanOffset(uint8_t& offset) -> Func {
 		return [&offset] (...) {
 			offset = 0;
 			return true;
 		};
 	}
+	// CHECK OFFSET
 	namespace Check {
 		static auto CheckOffset(uint8_t& offset) -> Func {
 			return [&offset] (...) {
 				return offset <= sizeof(uint64_t);
 			};
 		}
+		// OUT OF RANGE
 		static bool OutOfRange(uint8_t** ppbeg, uint8_t** ppend) {
 			uint8_t* pbeg{*ppbeg};
 			uint8_t* pend{*ppend};
 			return pbeg < pend;
 		}
+		// OUT OF RANGE
 		static auto OutOfRange() -> Func {
 			return [] (uint8_t** ppbeg, uint8_t** ppend) {
 				return OutOfRange(ppbeg, ppend);
 			};
 		}
-		static auto OutOfRange(uint8_t* pbeg) -> Func {
-			return [pbeg] (uint8_t** ppbeg, uint8_t** ppend) {
-				return pbeg < *ppbeg;
+		// OUT OF RANGE
+		static auto OutOfRange(size_t const beg, size_t const end) -> Func {
+			return [beg, end] (uint8_t**, uint8_t**) {
+				return (gpbeg + beg) < (gpend - end);
+			};
+		}
+		// OUT OR RANGE RIGHT IN BITS
+		static auto OutOfRangeRightInBits(size_t const offset) -> Func {
+			return [offset] (uint8_t** ppbeg, uint8_t**) {
+				return ((*ppbeg) + (size_t)(std::ceil(offset/8))) < gpend;
+			};
+		}
+		// OUT OR RANGE RIGHT
+		static auto OutOfRangeRight(size_t const offset) -> Func {
+			return [offset] (uint8_t** ppbeg, uint8_t**) {
+				return ((*ppbeg) + offset) < gpend;
+			};
+		}
+		// OUT OR RANGE LEFT
+		static auto OutOfRangeLeft(size_t const offset) -> Func {
+			return [offset] (uint8_t** ppbeg, uint8_t**) {
+				return gpbeg <= ((*ppbeg) - offset);
 			};
 		}
 	}
